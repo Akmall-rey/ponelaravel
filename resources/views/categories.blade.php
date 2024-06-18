28a0e5b0b4cedb3786affcbd45f6480090c59952
Akmall-rey <raihanakmal354@gmail.com>
2024-06-18 22:34:03 +0700

    updated

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -11,7 +11,9 @@
                 <div class="col-md-4">
                     <a href="/posts?category={{ $category->slug }}">
                         <div class="card text-bg-dark">
-                            <img src="https:/source.unsplash.com/500x500?{{ $category->name }}" class="card-img"
+                            {{-- <img src="https://picsum.photos/seed/{{ $post->category->name }}/500/400" class="card-img-top"
+                                    alt="{{ $post->category->name }}" class="img-fluid mt-3"> --}}
+                            <img src="https://picsum.photos/seed/{{ $category->name }}/500/500" class="card-img"
                                 alt="{{ $category->name }}">
                             <div class="card-img-overlay d-flex align-items-center p-0">
                                 <h5 class="card-title text-center flex-fill p-4 fs-3"
